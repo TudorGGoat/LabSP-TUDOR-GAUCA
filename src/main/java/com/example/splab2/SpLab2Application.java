ca9ced9430c23764d7633fb70ef3abc0993f86ee
Tudor <146804697+TudorGGoat@users.noreply.github.com>
2023-10-17 18:37:23 +0300

    .

---
+++ src/main/java/com/example/splab2/SpLab2Application.java
@@ -0,0 +1,13 @@
+package com.example.splab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpLab2Application.class, args);
+    }
+
+}
